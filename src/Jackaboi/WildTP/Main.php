eac03c3f96922ef7332f8b7a3aba985c2ec8e8b2
Silverz Playz <46418269+SilverzPlayz@users.noreply.github.com>
2019-09-30 16:58:06 +0100

    Update Main.php
    e

--- src/Jackaboi/WildTP/Main.php
+++ src/Jackaboi/WildTP/Main.php
@@ -14,7 +14,7 @@ use pocketmine\math\Vector3;
 class Main extends PluginBase {
 
 	public function onEnable() {
-		$this->getLogger()->info("WildTP has been enabled!");
+		$this->getLogger()->info("Wild has been enabled!");
 	}
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $param ) :bool{
@@ -22,19 +22,18 @@ class Main extends PluginBase {
 			case "wild":
 				if($sender->hasPermission("WildTP.wild")) {
 					if($sender instanceof Player) {
-						$x = rand(1,350000);
-            					$y = rand(1,256);
-						$z = rand(1,350000);
-						$sender->teleport($sender->getLevel()->getSafeSpawn(new Vector3($x, $y, $z)));
-						$sender->sendTip("§b[WildTP] You have been teleported somewhere in the wild!");
-						$sender->sendMessage("§b[WildTP] teleporting to: $x, $y, $z");
+						$x = rand(1,3500);
+						$z = rand(1,3500);
+						$sender->teleport($sender->getLevelByName("factionspawn")->getSafeSpawn(new Vector3($x, 128, $z)));
+						$sender->addTitle("§6§lWILDERNESS");
+						$sender->sendMessage("§c§lWARNING§7 Be careful, Pvp is enabled");
 					}
 					else {
-						$sender->sendMessage("[WildTP] This command can only be run in-game!");
+						$sender->sendMessage("Thiscommand can only be run in-game!");
 					}
 				}
 				else {
-					$sender->sendMessage("[WildTP] You do not have permission to run this command!");
+					$sender->sendMessage("§cYou do not have permission to run this command!");
 				}
 				return true;
 			break;
@@ -42,7 +41,7 @@ class Main extends PluginBase {
 		}
 	}
 	public function onDisable() {
-		$this->getLogger()->info("WildTP has been disabled!");
+		$this->getLogger()->info("Wild has been disabled!");
 	}
 
 }
